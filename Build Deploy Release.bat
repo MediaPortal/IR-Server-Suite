723c92ec22f6cad3a62353e0ac029d03a083ce41
chefkoch <chefkoch@team-mediaportal.com>
2008-04-03 01:18:53 +0000

    reverted last change, was stupid and did not really thought about it, soory :(

--- Build Deploy Release.bat
+++ Build Deploy Release.bat
@@ -2,7 +2,7 @@ rem @ECHO OFF
 
 
 "%ProgramFiles%\Microsoft Visual Studio 8\Common7\IDE\devenv.com" /rebuild Release "IR Server Suite.sln"
-"%ProgramFiles%\NSIS\makensis.exe" setup\setup.nsi
+"%ProgramFiles%\NSIS\makensis.exe" setup.nsi
 
 EXIT
 
