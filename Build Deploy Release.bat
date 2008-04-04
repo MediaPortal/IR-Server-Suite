d0771242b4b9affebebac16a878d6ab39c5f1a46
and-81 <and-81@team-mediaportal.com>
2008-04-04 16:08:12 +0000

    added help compile step

--- Build Deploy Release.bat
+++ Build Deploy Release.bat
@@ -1,2 +1,3 @@
 "%ProgramFiles%\Microsoft Visual Studio 8\Common7\IDE\devenv.com" /rebuild Release "IR Server Suite.sln"
+"%ProgramFiles%\HTML Help Workshop\hhc.exe" "Documentation\IR Server Suite.hhp"
 "%ProgramFiles%\NSIS\makensis.exe" setup\setup.nsi
