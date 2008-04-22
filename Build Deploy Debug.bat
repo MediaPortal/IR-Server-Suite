e0ee0b5ecdad0dd7b7fd9699641bc7d576bd8e1f
chefkoch <chefkoch@team-mediaportal.com>
2008-04-22 14:23:50 +0000

    added status msgs to build scripts

--- Build Deploy Debug.bat
+++ Build Deploy Debug.bat
@@ -1,14 +1,23 @@
-rem @ECHO OFF
+@ECHO OFF
 
+echo.
+echo -= IR Server Suite : Build Deploy Debug.bat =-
+
+echo.
+echo Writing SVN revision assemblies...
 setup\DeployVersionSVN.exe /svn="%CD%"  > build_debug.log
 
+echo.
+echo Building IR Server Suite...
 "%ProgramFiles%\Microsoft Visual Studio 8\Common7\IDE\devenv.com" /rebuild Debug "IR Server Suite.sln" >> build_debug.log
 
+echo.
+echo Reverting assemblies...
 setup\DeployVersionSVN.exe /svn="%CD%" /revert >> build_debug.log
 
-rem these commands are necessary to get the svn revision, to enable them just remove the EXIT one line above
+echo.
+echo Building Installer...
 setup\DeployVersionSVN.exe /svn="%CD%" /GetVersion >> build_debug.log
-
 IF NOT EXIST version.txt EXIT
 SET /p version=<version.txt
 DEL version.txt
--- Build Deploy Release.bat
+++ Build Deploy Release.bat
@@ -1,3 +1,16 @@
+@ECHO OFF
+
+echo.
+echo -= IR Server Suite : Build Deploy Release.bat =-
+
+echo.
+echo Building IR Server Suite...
 "%ProgramFiles%\Microsoft Visual Studio 8\Common7\IDE\devenv.com" /rebuild Release "IR Server Suite.sln" > build_release.log
+
+echo.
+echo Building Help file...
 "%ProgramFiles%\HTML Help Workshop\hhc.exe" "Documentation\IR Server Suite.hhp" >> build_release.log
+
+echo.
+echo Building Installer...
 "%ProgramFiles%\NSIS\makensis.exe" setup\setup.nsi >> build_release.log
