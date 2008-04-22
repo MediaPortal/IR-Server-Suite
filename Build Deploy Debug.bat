9d8ea5847f10c894ddc239e67e3b0e97ebed006f
chefkoch <chefkoch@team-mediaportal.com>
2008-04-22 14:21:25 +0000

    output for build script is written to logfile

--- Build Deploy Debug.bat
+++ Build Deploy Debug.bat
@@ -1,15 +1,15 @@
 rem @ECHO OFF
 
-setup\DeployVersionSVN.exe /svn="%CD%"
+setup\DeployVersionSVN.exe /svn="%CD%"  > build_debug.log
 
-"%ProgramFiles%\Microsoft Visual Studio 8\Common7\IDE\devenv.com" /rebuild Debug "IR Server Suite.sln"
+"%ProgramFiles%\Microsoft Visual Studio 8\Common7\IDE\devenv.com" /rebuild Debug "IR Server Suite.sln" >> build_debug.log
 
-setup\DeployVersionSVN.exe /svn="%CD%" /revert
+setup\DeployVersionSVN.exe /svn="%CD%" /revert >> build_debug.log
 
 rem these commands are necessary to get the svn revision, to enable them just remove the EXIT one line above
-setup\DeployVersionSVN.exe /svn="%CD%" /GetVersion
+setup\DeployVersionSVN.exe /svn="%CD%" /GetVersion >> build_debug.log
 
 IF NOT EXIST version.txt EXIT
 SET /p version=<version.txt
 DEL version.txt
-"%ProgramFiles%\NSIS\makensis.exe" /DVER_BUILD=%version% setup\setup.nsi
+"%ProgramFiles%\NSIS\makensis.exe" /DVER_BUILD=%version% setup\setup.nsi >> build_debug.log
--- Build Deploy Release.bat
+++ Build Deploy Release.bat
@@ -1,3 +1,3 @@
-"%ProgramFiles%\Microsoft Visual Studio 8\Common7\IDE\devenv.com" /rebuild Release "IR Server Suite.sln"
-"%ProgramFiles%\HTML Help Workshop\hhc.exe" "Documentation\IR Server Suite.hhp"
-"%ProgramFiles%\NSIS\makensis.exe" setup\setup.nsi
+"%ProgramFiles%\Microsoft Visual Studio 8\Common7\IDE\devenv.com" /rebuild Release "IR Server Suite.sln" > build_release.log
+"%ProgramFiles%\HTML Help Workshop\hhc.exe" "Documentation\IR Server Suite.hhp" >> build_release.log
+"%ProgramFiles%\NSIS\makensis.exe" setup\setup.nsi >> build_release.log
