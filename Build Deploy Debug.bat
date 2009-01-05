e448032a7688710c15740208ebe4da12d764e08a
chefkoch <chefkoch@team-mediaportal.com>
2009-01-05 18:12:25 +0000

    added a variable for the build log file added a variable for the build type -> makes the build scripts more flexible, maybe we want to merge both into one ? and only call it with an additional debug parameter to create a debug build?

--- Build Deploy Debug.bat
+++ Build Deploy Debug.bat
@@ -1,29 +1,41 @@
 @ECHO OFF
 
-REM Select program path based on current machine environment
 
+REM set build type to debug
+set BUILD_TYPE=Debug
+
+
+REM set logfile where the infos are written to, and clear that file
+set LOG=build_%BUILD_TYPE%.log
+echo. > %LOG%
+
+
+REM Select program path based on current machine environment
 set progpath=%ProgramFiles%
 if not "%ProgramFiles(x86)%".=="". set progpath=%ProgramFiles(x86)%
 
+
 echo.
-echo -= IR Server Suite : Build Deploy Debug.bat =-
+echo -= IR Server Suite =-
+echo -= build mode: %BUILD_TYPE% =-
+echo.
 
 echo.
 echo Writing SVN revision assemblies...
-setup\DeployVersionSVN.exe /svn="%CD%"  > build_debug.log
+setup\DeployVersionSVN.exe /svn="%CD%"  >> %LOG%
 
 echo.
 echo Building IR Server Suite...
-"%progpath%\Microsoft Visual Studio 8\Common7\IDE\devenv.com" /rebuild Debug "IR Server Suite.sln" >> build_debug.log
+"%progpath%\Microsoft Visual Studio 8\Common7\IDE\devenv.com" /rebuild %BUILD_TYPE% "IR Server Suite.sln" >> %LOG%
 
 echo.
 echo Reverting assemblies...
-setup\DeployVersionSVN.exe /svn="%CD%" /revert >> build_debug.log
+setup\DeployVersionSVN.exe /svn="%CD%" /revert >> %LOG%
 
 echo.
 echo Building Installer...
-setup\DeployVersionSVN.exe /svn="%CD%" /GetVersion >> build_debug.log
+setup\DeployVersionSVN.exe /svn="%CD%" /GetVersion >> %LOG%
 IF NOT EXIST version.txt EXIT
 SET /p version=<version.txt
 DEL version.txt
-"%progpath%\NSIS\makensis.exe" /DVER_BUILD=%version% setup\setup.nsi >> build_debug.log
+"%progpath%\NSIS\makensis.exe" /DVER_BUILD=%version% setup\setup.nsi >> %LOG%
--- Build Deploy Release.bat
+++ Build Deploy Release.bat
@@ -1,21 +1,39 @@
 @ECHO OFF
 
-REM Select program path based on current machine environment
 
+REM detect if BUILD_TYPE should be release or debug
+if not %1!==Debug! goto RELEASE
+:DEBUG
+set BUILD_TYPE=Debug
+goto END_BUILD_TYPE
+:RELEASE
+set BUILD_TYPE=Release
+:END_BUILD_TYPE
+
+
+REM set logfile where the infos are written to, and clear that file
+set LOG=build_%BUILD_TYPE%.log
+echo. > %LOG%
+
+
+REM Select program path based on current machine environment
 set progpath=%ProgramFiles%
 if not "%ProgramFiles(x86)%".=="". set progpath=%ProgramFiles(x86)%
 
+
 echo.
-echo -= IR Server Suite : Build Deploy Release.bat =-
+echo -= IR Server Suite =-
+echo -= build mode: %BUILD_TYPE% =-
+echo.
 
 echo.
 echo Building IR Server Suite...
-"%progpath%\Microsoft Visual Studio 8\Common7\IDE\devenv.com" /rebuild Release "IR Server Suite.sln" > build_release.log
+"%progpath%\Microsoft Visual Studio 8\Common7\IDE\devenv.com" /rebuild %BUILD_TYPE% "IR Server Suite.sln" >> %LOG%
 
 echo.
 echo Building Help file...
-"%ProgramFiles%\HTML Help Workshop\hhc.exe" "Documentation\IR Server Suite.hhp" >> build_release.log
+"%ProgramFiles%\HTML Help Workshop\hhc.exe" "Documentation\IR Server Suite.hhp" >> %LOG%
 
 echo.
 echo Building Installer...
-"%progpath%\NSIS\makensis.exe" setup\setup.nsi >> build_release.log
+"%progpath%\NSIS\makensis.exe" setup\setup.nsi >> %LOG%
